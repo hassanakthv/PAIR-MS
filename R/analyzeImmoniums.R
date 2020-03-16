ab7f2ed2643c80962e19fc25829cde6f6bf4ca21
Hassan Gharibi <43888767+hassanakthv@users.noreply.github.com>
2020-03-16 11:29:57 +0100

    Update analyzeImmoniums.R

--- R/analyzeImmoniums.R
+++ R/analyzeImmoniums.R
@@ -267,3 +267,53 @@ analyze_immoniums_2 <- function(file, width=0.001, ions=AAIons, fixSigma=T){
   result
 }
        
+
+analyze_immoniums__ <- function(file, width=0.001, ions=AAIons, fixSigma=T){
+  message(sprintf("Reading file [%s]", file))
+  msrun <- openMSfile(file, backend = "Ramp")
+  hd <- header(msrun)
+  immscans <- which(hd$msLevel==2)
+  message(sprintf("\t%d immonium scans found", length(immscans)))
+  result <- data.frame()
+  message("Processing:")
+
+  hd %>%
+    filter(seqNum %in% immscans) %>%
+    group_by(seqNum) %>%
+    do({
+      ires <- data.frame()
+      dd = .
+      ii <- dd$seqNum[[1]]
+      ss <- peaks(msrun, ii)
+      ss_range <- diff(range(ss[,1]))
+      for(ion_ in names(ions)){
+        mz = monoMass(ions[[ion_]])
+        peaks <- c("13C")
+        if("N" %in% names(ions[[ion_]]) & ions[[ion_]]["N"] > 0)
+          peaks <- c(peaks, "15N")
+        if("H" %in% names(ions[[ion_]]) & ions[[ion_]]["H"] > 0)
+          peaks <- c(peaks, "2H")
+        if("O" %in% names(ions[[ion_]]) & ions[[ion_]]["O"] > 0)
+          peaks <- c(peaks, "18O")
+        if(ss_range>5){
+          cres <- get_isopeaks(ss, mz, width=width, npoint=6, fixSigma=fixSigma, peaks=peaks)
+        }else{
+          cres <- get_isopeaks_nomono(ss, mz, width=width, npoint=6, fixSigma=fixSigma)
+        }
+        if(nrow(cres)>1)
+          ires <- ires %>%
+            bind_rows(
+              cres %>%
+                mutate(
+                  rt = dd$retentionTime[[1]],
+                  tic = dd$totIonCurrent[[1]],
+                  bpI = dd$basePeakIntensity[[1]],
+                  bpMZ = dd$basePeakMZ[[1]],
+                  ion=ion_)
+            )
+      }
+      ires$n <- apply(ires, 1, function(x)ions[[x["ion"]]][sub("\\d+","",x["peak"])])
+      ires
+    }) -> result
+  result
+}       
